003025c3b14aed2d326b46d3f6f17cb96f80e867
D3viii <devinsdevs@gmail.com>
2021-01-14 11:20:06 -0500

    Building the booking form

---
+++ src/pages/Booking/Booking.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Booking(props) {
+    return (
+        <form className="search">
+            <div className="form-group">
+                <label>Flights</label>
+                <input ></input>
+            </div>
+        </form>
+    )
+}
