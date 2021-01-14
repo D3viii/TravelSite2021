89275bc5b05c9d4be5bf5d56dca8d42b1175e3d1
D3viii <devinsdevs@gmail.com>
2021-01-14 11:40:25 -0500

    still building

--- src/pages/Booking/Booking.js
+++ src/pages/Booking/Booking.js
@@ -1,12 +1,21 @@
 import React from "react";
 
 function Booking(props) {
-    return (
-        <form className="search">
-            <div className="form-group">
-                <label>Flights</label>
-                <input ></input>
-            </div>
-        </form>
-    )
+  return (
+    <form className="search">
+      <div className="form-group">
+        <label>Flights</label>
+      </div>
+      <div className="col-sm">
+        <input type="text" placeholder="From" aria-label="From"></input>
+        <input type="text" placeholder="To" aria-label="To"></input>
+      </div>
+    </form>
+  );
+}
+
+{
+  /* <div class="col-sm">
+    <input type="text" class="form-control" placeholder="State" aria-label="State">
+  </div> */
 }
